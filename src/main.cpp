21124ec43a9d3ea8cba955163a27099181bec269
dingy6269 <gearonix.twink@gmail.com>
2025-10-10 21:35:55 +0000

    v8 sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,6 @@
-#include <CLI/CLI.hpp>
-#include <iostream>
-
+#include "sample.h"
 
 int main(int argc, char* argv[]) {
-    
-    CLI::App app {"My CLI tool"};
 
-    std::string name;
-    bool shout = false;
-
-    CLI::Option* option = app.add_option("-n,--name", name, "Name of the user");
-
-    option->default_val("Anonymous");
-
-    app.add_flag("-s, --shout", shout, "Uppercase output");
-
-    
-    std::cout << name << std::endl;
-
-    CLI11_PARSE(app, argc, argv);
-
-
-    std::cout << name << std::endl;
-
-    if (name.empty()) {
-        std::cout << "No name provided. \n";
-        return 1;
-    }
-
-    if (shout) {
-        for (auto& c: name) {
-            c = static_cast<char>(std::toupper(static_cast<unsigned char>(c)));
-        }
-    }
-
-    std::cout << "Hello, " << name << "!\n";
-    return 0;
+  return invoke_v8_sample(argc, argv);
 }
---
+++ src/sample.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+int invoke_v8_sample(int argc, char* argv[]);
