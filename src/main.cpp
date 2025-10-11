833acbf26024b2c45f4c9766f07452ecb16fb4d3
dingy6269 <gearonix.twink@gmail.com>
2025-10-11 10:53:52 +0000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <CLI/CLI.hpp>
 #include "sample.h"
+#include <CLI/CLI.hpp>
 
 #include <map>
 #include <string>
@@ -15,14 +15,47 @@
 using std::map;
 using std::string;
 
+namespace cli_defaults {
+  inline constexpr const char *APP_NAME = "cherry";
+  inline constexpr const char *DEFAULT_FILENAME = "index.js";
+} // namespace cli_defaults
 
-struct CliConfig {
-  std::string filename;
+class CliConfig {
+public:
+  static CliConfig parse(int argc, char *argv[]) {
+    CliConfig cfg;
+
+    map<string, string> options;
+
+    CLI::App app{cli_defaults::APP_NAME};
+
+    auto *opt = app.add_option("-n, --name", cfg.filename_, "file name")
+                    ->default_val(cli_defaults::DEFAULT_FILENAME);
+
+    try {
+      app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+      throw;
+    }
+
+    return cfg;
+  }
+
+  void verify() const {
+    if (filename_.empty()) {
+      throw std::runtime_error("No script was specified. \n");
+    }
+  }
+
+  const std::string &filename() const { return filename_; }
+
+private:
+  std::string filename_ = cli_defaults::DEFAULT_FILENAME;
 };
 
 class V8Runtime {
-  public:
-  explicit V8Runtime(const char* argv0) {
+public:
+  explicit V8Runtime(const char *argv0) {
     v8::V8::InitializeICUDefaultLocation(argv0);
     v8::V8::InitializeExternalStartupData(argv0);
     platform_ = v8::platform::NewDefaultPlatform();
@@ -36,55 +69,23 @@ class V8Runtime {
     v8::V8::DisposePlatform();
   }
 
-  private:
-    std::unique_ptr<v8::Platform> platform_;
+private:
+  std::unique_ptr<v8::Platform> platform_;
 
-  V8Runtime(const V8Runtime&) = delete;
-  V8Runtime& operator=(const V8Runtime&) = delete;
+  V8Runtime(const V8Runtime &) = delete;
+  V8Runtime &operator=(const V8Runtime &) = delete;
 };
-
 
 // int ParseOptions(int argc, ) {
 
 // }
 
-
-namespace cli_defaults {
-  inline constexpr const char* APP_NAME = "cherry";
-  inline constexpr const char* DEFAULT_FILENAME = "index.js";
-}
-
-CliConfig parse_cli(int argc, char* argv[])  {
-  map<string, string> options;
-
-  CliConfig cfg;
-  CLI::App app {cli_defaults::APP_NAME};
-
-  auto* opt = app.add_option("-n, --name", cfg.filename, "file name")
-  -> default_val(cli_defaults::DEFAULT_FILENAME);
-  
-  try {
-    app.parse(argc, argv);
-  } catch (const CLI::ParseError& e) {
-    throw;
-  }
-  
-  return cfg;
-}
-
-
-void verify_config(CliConfig cfg) {
-  if (cfg.filename.empty()) {
-    std::cout << "No filename provided." << std::endl;
-    return;
-  };
-}
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   V8Runtime v8(argv[0]);
-  auto cfg = parse_cli(argc, argv);
 
-  verify_config(cfg);
+  auto cfg = CliConfig::parse(argc, argv);
 
-  return invoke_v8_sample(cfg.filename, argv);
+  cfg.verify();
+
+  return invoke_v8_sample(cfg.filename(), argv);
 }
